63bd4e625b5710ee8273f5d463cceb6364b2b22b
App Academy Student <student@appacademy.io>
2016-09-16 15:59:02 -0700

    create Board class

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,1 +1,14 @@
-#knights
+class Board
+  attr_reader :board
+  def initialize
+    @board = Array.new(8) { Array.new(8) {"*"} }
+  end
+
+  def render
+    puts "  " + "#{(0..7).to_a.join(" ")}"
+    @board.each_with_index do |row, index|
+      puts "#{index} #{row.join(" ")}"
+    end
+    nil
+  end
+end
